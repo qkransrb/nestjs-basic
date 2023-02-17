34b8e8e75288f7542061c51208b6833490b7785b
qkransrb <qkransrb90@gmail.com>
2023-02-17 18:09:08 +0900

    logger middleware, http-exception filter, interceptor, pipes ...

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseInterceptors,
+} from '@nestjs/common';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+import { PositiveIntPipe } from 'src/common/pipes/positiveInt.pipe';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+@UseInterceptors(SuccessInterceptor)
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getCats() {
+    return 'all cats';
+  }
+
+  @Get(':id')
+  getCat(@Param('id', ParseIntPipe, PositiveIntPipe) id: number) {
+    return `cat ${id}`;
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return 'update partial cat';
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete cat';
+  }
+}
